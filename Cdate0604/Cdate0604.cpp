77500f09d00fd58ec04912a7f8f4bd0fb6ddaed0
20172104255 <1479880655@qq.com>
2018-06-04 17:36:06 +0800

    Cdate
    修改了一下12月31号的时候输出下一天的年月日

--- Cdate0604/Cdate0604.cpp
+++ Cdate0604/Cdate0604.cpp
@@ -1,6 +1,7 @@
 // Cdate0604.cpp : 定义控制台应用程序的入口点。
 //
 
+
 #include "stdafx.h"
 #include<iostream>
 using namespace std;
@@ -23,22 +24,18 @@ void Cdate::init(int y, int m, int d)
 }
 void Cdate::nextday()
 {
-	if (month == 4 || month == 6 || month == 9 || month == 11)
-	{
-		if (day == 30)
-		{
-			month = month + 1;
-			day = day + 1 - 30;
-		}
-	}
 	if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
 	{
-		if (month = 2)
+		if (month == 2)
 		{
-			if (day + 1 > 29)
+			if (day == 29)
 			{
 				month = month + 1;
 				day = day + 1 - 29;
+			}
+			else
+			{
+				day += 1;
 			}
 		}
 	}
@@ -46,25 +43,53 @@ void Cdate::nextday()
 	{
 		if (month == 2)
 		{
-			if (day + 1 > 28)
+			if (day == 28)
 			{
 				month = month + 1;
 				day = day + 1 - 28;
 			}
+			else
+			{
+				day += 1;
+			}
 		}
 	}
-	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+	if (month == 4 || month == 6 || month == 9 || month == 11)
+	{
+		if (day == 30)
+		{
+			month = month + 1;
+			day = day + 1 - 30;
+		}
+		else
+		{
+			day += 1;
+		}
+	}
+	else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10)
 	{
 		if (day == 31)
 		{
 			month = month + 1;
 			day = day + 1 - 31;
 		}
+		else
+		{
+			day += 1;
+		}
 	}
-	if (month > 12)
+	else if (month == 12)
 	{
-		month = month - 12;
-		year = year + 1;
+		if (day == 31)
+		{
+			day = day + 1 - 31;
+			month = month + 1 - 12;
+			year = year + 1;
+		}
+		else
+		{
+			day += 1;
+		}
 	}
 }
 void Cdate::display()
@@ -74,7 +99,9 @@ void Cdate::display()
 int main()
 {
 	Cdate oc;
-	oc.init(2018, 2, 28);
+	int a, b, c;
+	cin >> a >> b >> c;
+	oc.init(a, b, c);
 	oc.nextday();
 	oc.display();
 	return 0;
